6107e9979487fa2f533b1112f5f4dc03d5899d49
theoliss <theo.lissarrague@gmail.com>
2023-10-09 16:10:31 +0200

    modified:   client/client.py

--- client/client.py
+++ client/client.py
@@ -1,1 +1,17 @@
+import socket 
 
+sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sck.connect(('127.0.0.1', 8888))
+
+print("Connected")
+
+while True : 
+    to_send = input("What do you want to send : ? \0")
+
+    if(to_send == ""):
+        break
+
+    sck.send(to_send.encode())
+    print("ok sended well !")
+
+print("Done")
