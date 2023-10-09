d3bec2bdb44cceb541b525775a7d964c6a66a531
xRayz3n <sebastien_lam@hotmail.fr>
2023-10-09 16:25:30 +0200

    test

--- server/server.py
+++ server/server.py
@@ -2,17 +2,18 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 8888))
+server_socket.bind(('172.21.72.112', 8887))
 server_socket.listen()
 client_socket, client_addr = server_socket.accept()
 print(f'New connection from {client_addr}')
 
+input = ""
 while True:
-    input = ""
     car = client_socket.recv(1).decode("utf-8")
     if car == "\0":
         break
     input += car
+print(input)
 
 client_socket.close()
 server_socket.close()
