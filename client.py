229734029b3666534e01821e89b3941dc8906818
xRayz3n <sebastien_lam@hotmail.fr>
2023-10-10 17:51:31 +0200

    implemented packets into server.py

--- client.py
+++ client.py
@@ -2,12 +2,12 @@ import socket
 import packets
 
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sck.connect(('172.21.72.112', 8887))
+sck.connect(('172.21.72.112', 8888))
 
 print("Connected")
 
 while True : 
     to_send = input("Set your nickname: ")
     nickname_packet = packets.Packets(to_send,package_type='I')
-    sck.send()
+    nickname_packet.send(sck)
     print("ok sent well !")
