e1292bf1cdad9edb2e6eb3f765aebb2eadd51448
theoliss <theo.lissarrague@gmail.com>
2023-10-10 17:52:59 +0200

    a

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import packets
 
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sck.connect(('172.21.72.112', 8887))
+sck.connect(('172.21.72.112', 8886))
 
 print("Connected")
 
