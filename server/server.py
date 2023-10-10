e2bc20c036dd6dd3aa7d4cf43c15816ec4ee6e4e
xRayz3n <sebastien_lam@hotmail.fr>
2023-10-10 11:58:45 +0200

    changes

--- server/server.py
+++ server/server.py
@@ -2,38 +2,43 @@ import socket
 import threading
 import multiprocessing
 import class_game
+import player
 
 playerConnection_queue = multiprocessing.Queue()
 
+playerList = []
+
 def OpenConnections(): #keeps the connection open
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('172.21.72.112', 8887))
+    server_socket.listen()
     while True:
-        
         print("Looking for the next player...")
-        server_socket.listen()
         client_socket, client_addr = server_socket.accept()
-        playerConnection_queue.put(client_socket)
         print(f'New connection from {client_addr}')
-        threading.Thread(group=None, target=GetPlayers,args=client_socket).start()
+        threading.Thread(group=None, target=GetPlayers,args=[client_socket,client_addr]).start()
 
-def GetPlayers(client_socket): #acquire player info
+def GetPlayers(client_socket, client_addr): #acquire player info
 
     nickname = client_socket.recv(16).decode("utf-8") #receive player nickname, 16 char max
-    print(nickname)
+    print(f"Ip {client_addr} named to {nickname}")
+    playerInfo = player.Player(client_socket, client_addr,nickname)
+    playerList.append(player)
+    while True:
+        status = client_socket.recv(8).decode("utf-8") #receive player status, wait for him to get ready
+        if len(client_socket.recv(1024)) == 0:
+            print(f"Connection lost from {playerInfo.name}")
+            client_socket.close()
+            break
+        
 
 
-connection_thread = threading.Thread(group=None, target=OpenConnections)
-connection_thread.start()
-"""
-input = ""
-while True:
-    car = client_socket.recv(1).decode("utf-8")
-    if car == "\0":
-        break
-    input += car
-print(input)
-"""
+
 
 #server_socket.close()
+
+
+if __name__ == '__main__':
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 8887))
+    connection_thread = threading.Thread(group=None, target=OpenConnections)
+    connection_thread.start()
